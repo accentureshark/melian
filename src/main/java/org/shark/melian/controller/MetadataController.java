1b93bdf83f2525e14ed7345307b0dbabd9be01c0
FabianAG <faguero@gmail.com>
2025-06-18 20:29:08 -0300

    feat: add metadata endpoint and generic service

--- src/main/java/org/shark/melian/controller/MetadataController.java
+++ src/main/java/org/shark/melian/controller/MetadataController.java
@@ -3,6 +3,8 @@ package org.shark.melian.controller;
 
 import org.shark.melian.model.TableShortDto;
 import org.shark.melian.service.MetadataService;
+import org.shark.melian.model.DatabaseMetadataDto;
+import org.shark.melian.model.McpMetadataDto;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,12 @@ import java.util.List;
 @RequestMapping("/mcp/metadata")
 public class MetadataController {
 
-    private final MetadataService sqlMetadataService;
-    private final MetadataService restApiMetadataService;
+    private final MetadataService<DatabaseMetadataDto> sqlMetadataService;
+    private final MetadataService<McpMetadataDto> restApiMetadataService;
 
     public MetadataController(
-            @Qualifier("sqlMetadataService") MetadataService sqlMetadataService,
-            @Qualifier("restApiMetadataService") MetadataService restApiMetadataService
+            @Qualifier("sqlMetadataService") MetadataService<DatabaseMetadataDto> sqlMetadataService,
+            @Qualifier("restApiMetadataService") MetadataService<McpMetadataDto> restApiMetadataService
     ) {
         this.sqlMetadataService = sqlMetadataService;
         this.restApiMetadataService = restApiMetadataService;
@@ -32,5 +34,18 @@ public class MetadataController {
             return restApiMetadataService.extractShortSummary();
         }
         return sqlMetadataService.extractShortSummary();
+    }
+
+    /**
+     * Returns the complete metadata description for the selected source.
+     * If {@code source=rest} a {@link McpMetadataDto} is returned, otherwise a
+     * {@link DatabaseMetadataDto}.
+     */
+    @GetMapping
+    public Object getMetadata(@RequestParam(name = "source", defaultValue = "sql") String source) {
+        if ("rest".equalsIgnoreCase(source)) {
+            return restApiMetadataService.extractMetadata();
+        }
+        return sqlMetadataService.extractMetadata();
     }
 }

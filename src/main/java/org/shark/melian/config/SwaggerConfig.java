3fab8b9490732780ee8cb0e2a659defdf97a4852
FabianAG <faguero@gmail.com>
2025-08-01 23:22:04 -0300

    feat: expose MCP endpoints via spring controller

---
+++ src/main/java/org/shark/melian/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package org.shark.melian.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+/**
+ * Basic OpenAPI configuration for Swagger UI.
+ */
+@OpenAPIDefinition(info = @Info(title = "Melian API", version = "1.0", description = "MCP server endpoints"))
+public class SwaggerConfig {
+}
+
